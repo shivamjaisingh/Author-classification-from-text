498522f11262c9dc08583a215a9dedd0d38d0459
Shivam Singh <shivamsingh94192@gmail.com>
2020-02-05 18:33:11 -0500

    Changed DB creation strategy and added genres. Used various algorithms to classify data.

--- csvCreator.py
+++ csvCreator.py
@@ -1,24 +1,68 @@
 import nltk
-import math
 import csv
-import numpy as np
+import pandas as pd
+from sklearn.utils import shuffle
 
-sent_array = []
-for book_name in nltk.corpus.gutenberg.fileids():
-    i = 0
+# Selecting seven books from Gutenberg digital books and seven different authors
+
+book_list = ["austen-emma.txt", "bible-kjv.txt",
+             "bryant-stories.txt", "chesterton-ball.txt",
+             "edgeworth-parents.txt", "melville-moby_dick.txt",
+             "whitman-leaves.txt"]
+
+# Adding genres to books manually
+
+genre = ["romance", "religion", "stories", "fiction", "children", "adventure", "poetry"]
+
+book_data = []
+
+i = 0
+
+
+# Defining get key function for sorted function
+def getKey(item):
+    return item[2]
+
+
+# Adding items to book_data list as  tuples(Book Name, Author, Number of words, Genre)
+
+for book_name in book_list:
+    t1 = (book_name, book_name.split("-")[0], len((nltk.corpus.gutenberg.raw(book_name)).split(" ")), genre[i])
+    i = i + 1
+    book_data.append(t1)
+
+# Sorting the book_data list by number of words in a book
+
+sorted(book_data, key=getKey)
+
+# printing the book_data list
+
+for book in book_data:
+    print(book[0], book[1], book[2], book[3])
+
+# Creating csv file
+
+rows = []   # creating list for rows
+
+for book in book_data:
+    s = 0
     n = 150
     t1 = ()
-    while i <= math.floor(len((nltk.corpus.gutenberg.raw(book_name)).split(" "))):
-        x = (nltk.corpus.gutenberg.raw(book_name).split()[i: i + n])
-        x1 = " ".join(x)
-        t1 = (x1, book_name.split("-")[0])
-        sent_array.append(t1)
-        i = i + n
+    j = 1
+    while j <= 200:
+        x = (nltk.corpus.gutenberg.raw(book[0]).split()[s: s + n])  # taking 150 words at a time
+        sentence_with_150_words = " ".join(x)             # sentence with 150 words
+        t1 = (book[1], book[3], sentence_with_150_words)  # tuple (Author, Genre, Sentence)
+        rows.append(t1)
+        s = s + n
+        j = j+1
 
-with open('file.csv', 'w') as out:
+with open('data_file.csv', 'w') as out:
     csv_out = csv.writer(out)
-    csv_out.writerow(['Sentence', 'Author'])
-    csv_out.writerows(sent_array)
+    csv_out.writerow(['Author', 'Genre', 'Sentence'])
+    csv_out.writerows(rows)
 
 
-np.random.shuffle(sent_array)
+
+
+
